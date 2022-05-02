3e1f2de8a98a68e1e682c866066f11c84fc6a6f1
kamkara <lnclass.kamkara@gmail.com>
2022-05-02 20:57:59 +0000

    add user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 
      #Add layouts content
     def title
-    base_title = "Action+  Missions humanitaires"
-    if @title.nil?
-    base_title
-    else
-    "#{@title} | #{base_title}"
-    end
+        base_title = "Action+  Missions humanitaires"
+        if @title.nil?
+        base_title
+        else
+        "#{@title} | #{base_title}"
+        end
     end
 
     #site description
@@ -18,5 +18,44 @@ module ApplicationHelper
     def keywords
     "Faire un don, don, association, ong, donner, don en ligne, don sécurisé, soutenir, donation, make a donation"
     end
+
+     #Sign in and up anywhere on app
+    def resource_name
+    :user
+  end
+  
+  def resource
+    @resource ||= User.new
+  end
+  
+  def resource_class
+    User
+  end
+  
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  #USER NOTICE
+  def bootstrap_class_for_flash(flash_type)
+    case flash_type
+    when 'success'
+      'alert-success'
+    when 'error'
+      'alert-danger'
+    when 'alert'
+      'alert-warning'
+    when 'notice'
+      'alert-info'
+    else
+      flash_type.to_s
+    end
+end
+
+
+# Notification
+  def render_notification
+    render "layouts/flash"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to:'welcome#index'
+  
+  devise_scope :user do
+    get 'profile/edit'    => 'devise/registrations#edit',   :as => :edit_user_registration
+    get 'profile/cancel'  => 'devise/registrations#cancel', :as => :cancel_user_registration
+    #get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  devise_for  :users,
+    :path => '',
+    :path_names =>
+      { :sign_in => 'connected',
+        :sign_out => 'logout',
+        :sign_up =>   '', :registration => 'adhesion',
+        :edit => 'edit'
+      }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
