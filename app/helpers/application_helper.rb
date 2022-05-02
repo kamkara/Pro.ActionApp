6920794ccaaeb2c0ad2857434d089047476f0f1f
kamkara <lnclass.kamkara@gmail.com>
2022-05-02 20:20:54 +0000

    add home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+     #Add layouts content
+    def title
+    base_title = "Action+  Missions humanitaires"
+    if @title.nil?
+    base_title
+    else
+    "#{@title} | #{base_title}"
+    end
+    end
+
+    #site description
+    def description
+    "Action+ ......"
+    end
+
+    def keywords
+    "Faire un don, don, association, ong, donner, don en ligne, don sécurisé, soutenir, donation, make a donation"
+    end
+
 end
